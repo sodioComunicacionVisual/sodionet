9e32538a8e44f1527c3032c68eb5e149412872ea
“Karen <karen.ortizg@yahoo.com>
2024-08-21 17:57:04 -0600

    New background animations to banner and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -812,3 +812,23 @@ document.addEventListener("DOMContentLoaded", function() {
     setAnimValues();
   }, 500); // Delay to ensure other animations are ready
 });
+
+
+//Confetti Text
+function confetti() {
+	$.each($(".particletext.confetti"), function(){
+	   var confetticount = ($(this).width()/50) * 10;
+	   for(var i = 0; i <= confetticount; i++) {
+		  $(this).append('<span class="particle c' + $.rnd(1,2) + '" style="top:' + $.rnd(10,50) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(6,8) + 'px; height:' + $.rnd(3,4) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+	   }
+	});
+ }
+ 
+ jQuery.rnd = function(m,n) {
+	m = parseInt(m);
+	n = parseInt(n);
+	return Math.floor(Math.random() * (n - m + 1)) + m;
+ }
+ 
+ confetti();
+ 
